e7a37935d8017f8d4aab6c5ee6bb3976807a5d24
itchyny <itchyny@hatena.ne.jp>
2017-01-25 00:38:37 +0900

    remove return value of codegen

--- minivm/codegen.go
+++ minivm/codegen.go
@@ -31,24 +31,21 @@ func (env *Env) addConst(value Value) int {
 	return len
 }
 
-func (env *Env) codegen(node Node) int {
-	var count int
+func (env *Env) codegen(node Node) {
 	switch node := node.(type) {
 	case Statements:
 		for _, stmt := range node.stmts {
-			count += env.codegen(stmt)
+			env.codegen(stmt)
 		}
 	case IfStmt:
-		count += env.codegen(node.expr)
+		env.codegen(node.expr)
 		jmpnot := env.addCode(Code{OpCode: OpJmpNot})
-		count++
-		count += env.codegen(node.stmts)
+		env.codegen(node.stmts)
 		if node.elsestmts != nil {
 			stmts, _ := node.elsestmts.(Statements)
 			jmp := env.addCode(Code{OpCode: OpJmp})
-			count++
 			env.code[jmpnot].Operand = len(env.code) - jmpnot - 1
-			count += env.codegen(stmts)
+			env.codegen(stmts)
 			env.code[jmp].Operand = len(env.code) - jmp - 1
 		} else {
 			env.code[jmpnot].Operand = len(env.code) - jmpnot - 1
@@ -59,16 +56,14 @@ func (env *Env) codegen(node Node) int {
 			fmt.Fprintln(os.Stderr, "unknown variable: "+node.ident)
 			os.Exit(1)
 		}
-		count += env.codegen(node.expr)
+		env.codegen(node.expr)
 		env.addCode(Code{OpCode: OpLetGVar, Operand: i})
-		count++
 	case PrintStmt:
-		count += env.codegen(node.expr)
+		env.codegen(node.expr)
 		env.addCode(Code{OpCode: OpPrint})
-		count++
 	case BinOpExpr:
-		count += env.codegen(node.left)
-		count += env.codegen(node.right)
+		env.codegen(node.left)
+		env.codegen(node.right)
 		var op int8
 		switch node.op {
 		case PLUS:
@@ -84,7 +79,6 @@ func (env *Env) codegen(node Node) int {
 			os.Exit(1)
 		}
 		env.addCode(Code{OpCode: op})
-		count++
 	case Ident:
 		i := env.vars.lookup(node.name)
 		if i < 0 {
@@ -92,24 +86,18 @@ func (env *Env) codegen(node Node) int {
 			os.Exit(1)
 		}
 		env.addCode(Code{OpCode: OpLoadGVar, Operand: i})
-		count++
 	case BoolExpr:
 		if node.value {
 			env.addCode(Code{OpCode: OpLoadT})
-			count++
 		} else {
 			env.addCode(Code{OpCode: OpLoadF})
-			count++
 		}
 	case IntExpr:
 		env.addCode(Code{OpCode: OpLoad, Operand: env.addConst(VInt{node.value})})
-		count++
 	case FloatExpr:
 		env.addCode(Code{OpLoad, env.addConst(VFloat{node.value})})
-		count++
 	default:
 		fmt.Fprintf(os.Stderr, "unknown node type: %+v\n", node)
 		os.Exit(1)
 	}
-	return count
 }
