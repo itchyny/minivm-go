fcb22ff0b2471c18a7f7e2b4ada5c76fe75d87d9
itchyny <itchyny@hatena.ne.jp>
2017-01-27 21:16:30 +0900

    move type Env to env.go

---
+++ minivm/env.go
@@ -0,0 +1,29 @@
+package minivm
+
+type Env struct {
+	pc       int
+	code     []Code
+	constant []Value
+	stack    Stack
+	vars     Vars
+	breaks   []int
+	conts    []int
+}
+
+func Codegen(node Node) *Env {
+	env := new(Env)
+	env.vars.alloc(node)
+	env.codegen(node)
+	return env
+}
+
+func (env *Env) addCode(code Code) int {
+	env.code = append(env.code, code)
+	return len(env.code) - 1
+}
+
+func (env *Env) addConst(value Value) int {
+	len := len(env.constant)
+	env.constant = append(env.constant, value)
+	return len
+}
