200c2315053fd5ee856201949a1e2f26ee60dd5b
itchyny <itchyny@hatena.ne.jp>
2017-01-28 01:12:21 +0900

    use flag package for -debug option

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"flag"
 	"os"
 
 	"github.com/itchyny/minivm-go/minivm"
 )
 
+var debug = flag.Bool("debug", false, "debug code instructions")
+
 func main() {
+	flag.Parse()
 	lexer := new(minivm.Lexer)
 	lexer.Init(os.Stdin)
 	minivm.Parse(lexer)
 	env := minivm.Codegen(lexer.Result())
-	if len(os.Args) > 1 && os.Args[1] == "--debug" {
+	if debug != nil && *debug {
 		env.Debug()
 	}
 	env.Execute()
